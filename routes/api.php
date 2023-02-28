ffbedd834807b1681a3e957bea0a40c25560222a
dumas2000 <40582404+dumas2000@users.noreply.github.com>
2023-02-28 11:39:37 -0500

    probando api para registro y login de usuario

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-
+Route::post('registro', [UserController::class,'registro']);
 Route::post('login', [UserController::class,'login']);
 
 Route::group(['middleware'=>'auth:api'], function(){
-    Route::post('registro', [UserController::class,'registro']);
+    
 });
